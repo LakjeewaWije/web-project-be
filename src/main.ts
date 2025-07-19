b7a016d54d69fb5e0af8f339846fc2b86a4b4eeb
LakjeewaWije <layerswije@gmail.com>
2025-07-20 00:57:36 +0100

    Setup : open api swagger documentation for the back end

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { initializeTransactionalContext } from 'typeorm-transactional';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   initializeTransactionalContext();
   const app = await NestFactory.create(AppModule);
+
+  // register swagger api
+  const config = new DocumentBuilder()
+    .setTitle('Web Project BE')
+    .setDescription('Web Project API description')
+    .setVersion('1.0')
+    .addTag('#Web Project BE')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
