085a7994f6a22310c13c3946f5482db3cf104793
LakjeewaWije <layerswije@gmail.com>
2025-07-20 23:35:58 +0100

    Added : brands and types of instruments they have

---
+++ src/utils/brandTypes.enum.ts
@@ -0,0 +1,11 @@
+export enum InstrumentBrands {
+  'Yamaha' = 1,
+  'Fender' = 2,
+  'Casio' = 3,
+  'Roland' = 4,
+  'Gibson' = 5,
+  'Rockstar' = 6,
+  'Paiste' = 7,
+  'Schecter' = 8,
+  'Behringer' = 9,
+}
---
+++ src/utils/categoryTypes.enum.ts
@@ -0,0 +1,12 @@
+export enum InstrumentCategories {
+  GUITAR = 1,
+  ORGAN = 2,
+  FLUTE = 3,
+  VIOLIN = 4,
+  MANDOLIN = 5,
+  SAXAPHONE = 6,
+  HARMONICA = 7,
+  HARP = 8,
+  PIANO = 9,
+  ACCORDIAN = 10,
+}
