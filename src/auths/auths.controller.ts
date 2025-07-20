5fc142e8c8571742f3c1929a2274da4eb97efcfc
LakjeewaWije <layerswije@gmail.com>
2025-07-21 00:06:53 +0100

    Added : singup user controller method for singup api /signup

--- src/auths/auths.controller.ts
+++ src/auths/auths.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
+import { AuthsService } from './auths.service';
+import { SignUpUserDto } from './dto/signup-user.dto';
 
 @Controller('auths')
-export class AuthsController {}
+export class AuthsController {
+  constructor(private readonly authService: AuthsService) {}
+
+  @Post('/signup')
+  async signUpUser(@Body() signUpUserDto: SignUpUserDto, @Req() req: Request) {
+    return { user: 'signed up' };
+  }
+}
