03384f1c5f096b9add65833904215bc376b1ed68
LakjeewaWije <layerswije@gmail.com>
2025-07-30 11:52:16 +0100

    Updated : login api controller methods with access token generation

--- src/auths/auths.controller.ts
+++ src/auths/auths.controller.ts
@@ -9,10 +9,14 @@ import { AuthsService } from './auths.service';
 import { SignUpUserDto } from './dto/signup-user.dto';
 import { Public } from 'src/utils/publicRequest.decorator';
 import { LoginUserDto } from './dto/login-user.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Controller('auths')
 export class AuthsController {
-  constructor(private readonly authService: AuthsService) {}
+  constructor(
+    private readonly authService: AuthsService,
+    private jwtService: JwtService,
+  ) {}
 
   @Post('/signup')
   async signUpUser(@Body() signUpUserDto: SignUpUserDto, @Req() req: Request) {
@@ -28,7 +32,24 @@ export class AuthsController {
   @Public()
   @Post('/login')
   async logInTrainer(@Body() logInUser: LoginUserDto, @Req() req: Request) {
-    let res = await this.authService.logInUser(logInUser);
+    let user = await this.authService.logInUser(logInUser);
+
+    let tokenPayload: any = {
+      userId: user.userId,
+      roles: user.roles,
+      firstName: user.firstName,
+      mobilePhone: user.mobilePhone,
+    };
+
+    const accessToken = await this.jwtService.signAsync(tokenPayload, {
+      expiresIn: '7d',
+    });
+
+    const res = {
+      user,
+      accessToken,
+    };
+
     return res;
   }
 }
--- src/auths/auths.module.ts
+++ src/auths/auths.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AuthsService } from './auths.service';
 import { AuthsController } from './auths.controller';
 import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      global: true,
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get('app.secret'),
+        signOptions: { expiresIn: '7d' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   providers: [AuthsService],
   controllers: [AuthsController],
 })
