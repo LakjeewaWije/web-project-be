eec510fdb691d99909e85ec6c04944084000b6d9
Dilusha Sindutharu <sindutharu821@gmail.com>
2025-08-03 17:11:29 +0100

    add product details with image is added to the service

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,51 @@
 import { Injectable } from '@nestjs/common';
-
+import { InjectRepository } from '@nestjs/typeorm';
+import { Product } from './entity/product.entity';
+import { Repository } from 'typeorm';
+import { Image } from './entity/image.entity';
+import { ProductDto } from './dto/add-product.dto';
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+  constructor(
+    @InjectRepository(Product)
+    private productsRepository: Repository<Product>,
+    @InjectRepository(Image)
+    private imagesRepository: Repository<Image>,
+  ) {}
+
+  async addProduct(dto: ProductDto): Promise<Product | any> {
+    try {
+      const addProductDao: Product = {
+        productName: dto.productName,
+        productDescription: dto.productDescription,
+        productBrand: dto.productBrand,
+        status: dto.status,
+        productCategory: dto.productCategory,
+        price: dto.price,
+        weight: dto.weight,
+        images: [],
+      };
+
+      const res = await this.productsRepository.save(addProductDao);
+
+      for (const image of dto.images as any) {
+        const imageDao: Image = {
+          product: res,
+          url: image,
+        };
+        await this.imagesRepository.save(imageDao);
+      }
+
+      const final = await this.productsRepository.findOne({
+        where: {
+          productId: res.productId,
+        },
+        relations: { images: true },
+      });
+
+      return final;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
