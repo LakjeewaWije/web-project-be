957adc6f9617ea821ea5d8799ab4a3b7194af471
romesh-CG <romeshperera97@gmail.com>
2025-08-08 02:28:27 +0100

    Added: new class to get products

---
+++ src/products/query/get-product.dto.ts
@@ -0,0 +1,50 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Transform } from 'class-transformer';
+import {
+  IsEnum,
+  IsInt,
+  IsOptional,
+  IsString,
+  Matches,
+  Max,
+  Min,
+} from 'class-validator';
+import { Role } from 'src/role/role.enum';
+import { InstrumentCategories } from 'src/utils/categoryTypes.enum';
+import { InstrumentBrands } from 'src/utils/brandTypes.enum';
+export class GetProductDto {
+  @ApiProperty({ required: false, enum: InstrumentCategories })
+  @IsOptional()
+  // @IsEnum(CategoryTypes)
+  @IsString()
+  productCategory?: InstrumentCategories;
+
+  @ApiProperty({ required: false, enum: InstrumentBrands })
+  @IsOptional()
+  // @IsEnum(BrandTypes)
+  @IsString()
+  productBrand?: InstrumentBrands;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  @Matches(/^[a-zA-Z]+,(asc|desc)$/i, {
+    message: 'Invalid sort parameter. Format should be "field,order".',
+  })
+  sort?: string;
+
+  @ApiProperty({ required: false, type: Number })
+  @IsOptional()
+  @Transform(({ value }) => parseInt(value, 10))
+  @IsInt()
+  @Min(1)
+  pageNumber?: number;
+
+  @ApiProperty({ required: false, type: Number })
+  @IsOptional()
+  @Transform(({ value }) => parseInt(value, 10))
+  @IsInt()
+  @Min(1)
+  @Max(1000)
+  pageSize?: number;
+}
